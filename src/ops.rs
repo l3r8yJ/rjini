1570272d0faf37c984ec67b0f71ada147db93097
Ivan <clicker.heroes.acg@gmail.com>
2023-02-22 00:27:29 +0300

    #replace_node

--- src/ops.rs
+++ src/ops.rs
@@ -1,8 +1,12 @@
-use std::ops::Add;
 use crate::RJini;
 use anyhow::anyhow;
 use anyhow::Result;
+use itertools::Itertools;
+use std::ops::Add;
 
+/// @todo #1 Harder unit tests.
+/// We have to make our tests as hard as possible.
+///
 /// Creating a new instance of RJini from a XPATH as string.
 impl RJini {
     /// It takes a string and returns a RJini object.
@@ -22,7 +26,9 @@ impl RJini {
     ///
     /// A struct with a body field.
     pub fn from(xpath: &str) -> Self {
-        RJini { xpath: xpath.to_string() }
+        RJini {
+            xpath: xpath.to_string(),
+        }
     }
 
     /// It adds a node to the body of the XPATH.
@@ -74,9 +80,47 @@ impl RJini {
     /// A new RJini object with the XPATH of the old one but with the node removed.
     pub fn remove_node(&self, node: &str) -> RJini {
         let b = self.xpath.replace(&node.to_string().add("/"), "");
-        RJini {
-            xpath: b
-        }
+        RJini { xpath: b }
+    }
+
+    /// > Replace a node in the xpath with a new node
+    ///
+    /// For example:
+    /// ```
+    /// use rjini::RJini;
+    /// let j = RJini::empty()
+    ///         .add_node("Ruby").unwrap()
+    ///         .add_node("is").unwrap()
+    ///         .add_node("a").unwrap()
+    ///         .add_node("bad").unwrap()
+    ///         .add_node("dog").unwrap()
+    ///         .replace_node("bad", "good")
+    ///         .xpath;
+    /// assert_eq!("Ruby/is/a/good/dog/", j);
+    /// ```
+    ///
+    /// Arguments:
+    ///
+    /// * `origin`: The node you want to replace.
+    /// * `new`: The new node name
+    ///
+    /// Returns:
+    ///
+    /// A new RJini object with the xpath replaced.
+    pub fn replace_node(&self, origin: &str, new: &str) -> RJini {
+        let x = self
+            .xpath
+            .split("/")
+            .map(|node| {
+                return if String::from(node).eq(origin) {
+                    new
+                } else {
+                    node
+                };
+            })
+            .join("/")
+            .to_string();
+        RJini { xpath: x }
     }
 }
 
@@ -107,5 +151,19 @@ fn checks_removes_node() -> Result<()> {
         .add_node("dog")?
         .remove_node("not");
     assert_eq!("Ruby/is/my/dog/", j.xpath);
+    Ok(())
+}
+
+#[test]
+fn checks_replaces_node() -> Result<()> {
+    let j = RJini::empty()
+        .add_node("Ruby")?
+        .add_node("is")?
+        .add_node("a")?
+        .add_node("bad")?
+        .add_node("dog")?
+        .replace_node("bad", "good")
+        .xpath;
+    assert_eq!("Ruby/is/a/good/dog/", j);
     Ok(())
 }
